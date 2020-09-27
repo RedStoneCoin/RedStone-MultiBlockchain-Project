18b8f414a425a96874254d5154d0aa62488099ea
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-09-27 08:03:00 -0400

    Add files via upload

---
+++ redstone.multichain/redstone.multichain.cpp
@@ -0,0 +1,13 @@
+﻿// redstone.multichain.cpp : Defines the entry point for the application.
+//RedStone Main.cpp
+
+#include "redstone.multichain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "REDSTONE MULTICHAIN." << endl;
+	cout << "Ver 0.1" << endl;
+	return 0;
+}
