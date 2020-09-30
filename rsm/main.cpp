73175cacbdb4e1910599c966540d190ba0a8e4b5
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-09-30 22:03:48 +0200

    Update main.cpp

--- rsm/main.cpp
+++ rsm/main.cpp
@@ -16,17 +16,25 @@ int main(int argc, char* argv[])
     cout << "sha256('" << input << "'):" << output1 << endl;
     return 0;
 }
-int main() {
-    Blockchain bChain = Blockchain();
+int count;
+    while(true)
+    {
+       Blockchain bChain = Blockchain(); //Loop to mine block every 10 sec loop will coint block that block that counted will be added to the cahin i think
+       block = cout << "Mining block" << endl; bChain.AddBlock(Block(count, "Block Data"));
+       count +=1; 
+       cout << block << endl; // print every 10 second 
+    }
+//int main() {
+ //   Blockchain bChain = Blockchain();
 
-    cout << "Mining block 1..." << endl;
-    bChain.AddBlock(Block(1, "Block 1 Data"));
+  //  cout << "Mining block 1..." << endl;
+ //  bChain.AddBlock(Block(1, "Block 1 Data"));
 
-    cout << "Mining block 2..." << endl;
-    bChain.AddBlock(Block(2, "Block 2 Data"));
+  //  cout << "Mining block 2..." << endl;
+  //  bChain.AddBlock(Block(2, "Block 2 Data"));
 
-    cout << "Mining block 3..." << endl;
-    bChain.AddBlock(Block(3, "Block 3 Data"));
+   // cout << "Mining block 3..." << endl;
+    //bChain.AddBlock(Block(3, "Block 3 Data"));
 
-    return 0;
-}
+   // return 0;
+//}
